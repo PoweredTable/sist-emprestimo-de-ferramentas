02bbe21ecbb4eb0cdae9584909e5d1a2490f1651
Lucas Peixer <Lucaspeicher@hotmail.com>
2024-05-01 18:05:14 -0300

    Inserção da tela de interface inicial

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,8 +1,12 @@
-package main.principal;
+package Principal;
+
+import view.TelaPrincipal;
 
 public class Principal {
 
     public static void main(String[] args) {
+       TelaPrincipal objeto = new TelaPrincipal();
+       objeto.setVisible(true);
+       
     }
-    
 }
