247e286dfd01700d423dc563521e16b5bd652063
WRbosss <waldyrprograming@gmail.com>
2024-05-11 21:22:14 -0300

    Organização realizada

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -2,12 +2,10 @@ package Principal;
 
 import visao.TelaPrincipal;
 
-
 public class Principal {
 
     public static void main(String[] args) {
        TelaPrincipal principal = new TelaPrincipal();
        principal.setVisible(true);
-
     }
 }
