9c3a936bfcb3cd9674b6a8fd057c9899f073f6c2
PoweredTable <poweredlucas@gmail.com>
2024-05-12 02:48:32 -0300

    feat: adiciona os controladores com grande parte das funcionalidades

--- src/main/java/controle/AmigoControle.java
+++ src/main/java/controle/AmigoControle.java
@@ -10,7 +10,21 @@ import util.TextUtil;
 
 public class AmigoControle {
 
+    public static void checkModelo(Amigo amigo) throws ExceptionDAO {
+        Objects.requireNonNull(amigo);
+        if (!TextUtil.isValidText(amigo.getNome())) {
+            throw new ExceptionDAO("Nome do amigo inválido!");
+        }
+        if (!TextUtil.isValidText(amigo.getApelido())) {
+            throw new ExceptionDAO("Apelido do amigo inválido!");
+        }
+        if (!TextUtil.isValidText(amigo.getTelefone())) {
+            throw new ExceptionDAO("Telefone do amigo inválido!");
+        }
+    }
+
     public static Optional<Amigo> buscar(Integer id) throws ExceptionDAO {
+        Objects.requireNonNull(id, "O Id do modelo 'Amigo' não pode ser nulo!");
         return Amigo.buscar(id);
     }
 
@@ -18,13 +32,36 @@ public class AmigoControle {
         return Amigo.buscarTudo();
     }
 
-    public static boolean cadastrar(String nome, String apelido, String telefone) throws ExceptionDAO {
-        if (!TextUtil.isValidTextList(List.of(nome, apelido, telefone))) {
-            return false;
-        }
+    public static void cadastrar(String nome, String apelido, String telefone) throws ExceptionDAO {
         Amigo amigo = new Amigo(nome, apelido, telefone);
+        cadastrar(amigo);
+    }
+
+    public static void cadastrar(Amigo amigo) throws ExceptionDAO {
+        checkModelo(amigo);
         Amigo.cadastrar(amigo);
-        return true;
+    }
+
+    public static void alterar(Amigo amigo) throws ExceptionDAO {
+        checkModelo(amigo);
+        Integer id = amigo.getId();
+        Objects.requireNonNull(id, "O Id do modelo 'Amigo' não pode ser nulo!");
+        int rowsAffected = Amigo.alterar(amigo);
+        if (rowsAffected == 0) {
+            throw new ExceptionDAO("O amigo '" + id + "' não foi excluído pois não existe!");
+        }
+    }
+
+    public static void excluir(Integer id) throws ExceptionDAO {
+        Objects.requireNonNull(id, "O Id do modelo 'Amigo' não pode ser nulo!");
+        int rowsAffected = Amigo.excluir(id);
+        if (rowsAffected == 0) {
+            throw new ExceptionDAO("O amigo '" + id + "' não foi excluído pois não existe!");
+        }
+    }
+
+    public static void excluir(Amigo amigo) throws ExceptionDAO {
+        excluir(amigo.getId());
     }
 
     public static boolean amigoPossuiEmprestimoAtivo(Integer id) throws ExceptionDAO {
--- src/main/java/controle/FerramentaControle.java
+++ src/main/java/controle/FerramentaControle.java
@@ -1,7 +1,7 @@
 package controle;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import dao.ExceptionDAO;
@@ -10,7 +10,18 @@ import util.TextUtil;
 
 public class FerramentaControle {
 
+    public static void checkModelo(Ferramenta ferramenta) throws ExceptionDAO {
+        Objects.requireNonNull(ferramenta);
+        if (!TextUtil.isValidText(ferramenta.getNome())) {
+            throw new ExceptionDAO("Nome da ferramenta inválido!");
+        }
+        if (!TextUtil.isValidText(ferramenta.getMarca())) {
+            throw new ExceptionDAO("Nome da ferramenta inválido!");
+        }
+    }
+
     public static Optional<Ferramenta> buscar(Integer id) throws ExceptionDAO {
+        Objects.requireNonNull(id);
         return Ferramenta.buscar(id);
     }
 
@@ -18,12 +29,44 @@ public class FerramentaControle {
         return Ferramenta.buscarTudo();
     }
 
-    public static boolean cadastrar(String nome, String marca, Double preco) throws ExceptionDAO {
-        if (!TextUtil.isValidTextList(List.of(nome, marca))) {
-            return false;
-        }
+    public static void cadastrar(String nome, String marca, Double preco) throws ExceptionDAO {
         Ferramenta ferramenta = new Ferramenta(nome, marca, preco);
+        cadastrar(ferramenta);
+    }
+
+    public static void cadastrar(Ferramenta ferramenta) throws ExceptionDAO {
+        checkModelo(ferramenta);
         Ferramenta.cadastrar(ferramenta);
-        return true;
+    }
+
+    public static void alterar(Ferramenta ferramenta) throws ExceptionDAO {
+        checkModelo(ferramenta);
+        Integer id = ferramenta.getId();
+        Objects.requireNonNull(id, "O Id do modelo 'Ferramenta' não pode ser nulo!");
+        int rowsAffected = Ferramenta.alterar(ferramenta);
+        if (rowsAffected == 0) {
+            throw new ExceptionDAO("A ferramenta '" + id + "' não foi alterada pois não existe!");
+        }
+    }
+
+    public static void excluir(Integer id) throws ExceptionDAO {
+        Objects.requireNonNull(id, "O Id do modelo 'Ferramenta' não pode ser nulo!");
+        int rowsAffected = Ferramenta.excluir(id);
+        if (rowsAffected == 0) {
+            throw new ExceptionDAO("A ferramenta '" + id + "' não foi excluída pois não existe!");
+        }
+    }
+
+    public static void excluir(Ferramenta ferramenta) throws ExceptionDAO {
+        excluir(ferramenta.getId());
+    }
+
+    public static Double getCustoTotal() throws ExceptionDAO {
+        ArrayList<Ferramenta> ferramentas = buscarTudo();
+        Double soma = 0.0;
+        for (Ferramenta ferramenta : ferramentas) {
+            soma += ferramenta.getPreco();
+        }
+        return soma;
     }
 }
