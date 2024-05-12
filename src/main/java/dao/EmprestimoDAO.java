28d7e350732366503253b4115f0bd468485a7eeb
PoweredTable <poweredlucas@gmail.com>
2024-05-12 02:44:39 -0300

    refactor: adiciona método private 'buscarEmprestimos' para remover código redundante

--- src/main/java/dao/EmprestimoDAO.java
+++ src/main/java/dao/EmprestimoDAO.java
@@ -39,14 +39,7 @@ public class EmprestimoDAO implements Dao<Emprestimo> {
         return Optional.empty();
     }
 
-    public ArrayList<Emprestimo> buscarTudo() throws ExceptionDAO {
-        String sql = "SELECT emprestimos.id as id_emprestimo, ferramentas.id as id_ferramenta, " +
-                     "amigos.id as id_amigo, data_inicial, data_prazo, data_devolucao, " +
-                     "ferramentas.nome AS nome_ferramenta, amigos.nome AS nome_amigo, apelido " +
-                     "FROM emprestimos " +
-                     "JOIN ferramentas ON emprestimos.id_ferramenta = ferramentas.id " +
-                     "JOIN amigos ON emprestimos.id_amigo = amigos.id " +
-                     "ORDER BY data_prazo ASC;";
+    private ArrayList<Emprestimo> buscarEmprestimos(String sql) throws ExceptionDAO {
         ArrayList<Emprestimo> emprestimos = new ArrayList<>();
         
         try (Connection connection = new DBConnection().getConnection();
@@ -74,48 +67,22 @@ public class EmprestimoDAO implements Dao<Emprestimo> {
         return emprestimos;
     }
 
-    public ArrayList<Emprestimo> buscarVencidos() throws ExceptionDAO {
-        String sql = "SELECT emprestimos.id as id_emprestimo, " +
-                "ferramentas.id as id_ferramenta, " +
-                "amigos.id as id_amigo, " +
-                "data_inicial, " +
-                "data_prazo, " +
-                "data_devolucao, " +
-                "ferramentas.nome AS nome_ferramenta, " +
-                "amigos.nome AS nome_amigo, " +
-                "apelido " +
+    public ArrayList<Emprestimo> buscarTudo() throws ExceptionDAO {
+        String sql = "SELECT emprestimos.id as id_emprestimo, ferramentas.id as id_ferramenta, " +
+                "amigos.id as id_amigo, data_inicial, data_prazo, data_devolucao, " +
+                "ferramentas.nome AS nome_ferramenta, amigos.nome AS nome_amigo, apelido " +
                 "FROM emprestimos " +
                 "JOIN ferramentas ON emprestimos.id_ferramenta = ferramentas.id " +
                 "JOIN amigos ON emprestimos.id_amigo = amigos.id " +
-                "WHERE data_prazo < CURRENT_DATE AND data_devolucao IS NULL " +
                 "ORDER BY data_prazo ASC;";
-        ArrayList<Emprestimo> emprestimos = new ArrayList<>();
-    
-        try (Connection connection = new DBConnection().getConnection();
-             PreparedStatement pStatement = connection.prepareStatement(sql);
-             ResultSet rs = pStatement.executeQuery()) {
-    
-            while (rs.next()) {
-                Emprestimo emprestimo = new Emprestimo();
-                emprestimo.setId(rs.getInt("id_emprestimo"));
-                emprestimo.setIdFerramenta(rs.getInt("id_ferramenta"));
-                emprestimo.setIdAmigo(rs.getInt("id_amigo"));
-                emprestimo.setDataInicial(toLocalDate(rs.getDate("data_inicial")));
-                emprestimo.setDataPrazo(toLocalDate(rs.getDate("data_prazo")));
-                emprestimo.setDataDevolucao(toLocalDate(rs.getDate("data_devolucao")));
-                emprestimo.setNomeFerramenta(rs.getString("nome_ferramenta"));
-                emprestimo.setNomeAmigo(rs.getString("nome_amigo"));
-                emprestimo.setApelidoAmigo(rs.getString("apelido"));
-                emprestimos.add(emprestimo);
-            }
-    
-        } catch (SQLException e) {
-            throw new ExceptionDAO("Erro ao consultar emprestimo: " + e);
-        }
-    
-        return emprestimos;
+        return buscarEmprestimos(sql);
     }
-    
+
+    public ArrayList<Emprestimo> buscarAtivos() throws ExceptionDAO {
+        //TODO: Implementar query para buscar ativos.
+        return buscarTudo();
+    }
+
     public ArrayList<Emprestimo> buscarEmDia() throws ExceptionDAO {
         String sql = "SELECT emprestimos.id as id_emprestimo, " +
                 "ferramentas.id as id_ferramenta, " +
@@ -131,35 +98,26 @@ public class EmprestimoDAO implements Dao<Emprestimo> {
                 "JOIN amigos ON emprestimos.id_amigo = amigos.id " +
                 "WHERE data_prazo >= CURRENT_DATE AND data_devolucao IS NULL " +
                 "ORDER BY data_prazo ASC;";
-        ArrayList<Emprestimo> emprestimos = new ArrayList<>();
-    
-        try (Connection connection = new DBConnection().getConnection();
-             PreparedStatement pStatement = connection.prepareStatement(sql);
-             ResultSet rs = pStatement.executeQuery()) {
-    
-            while (rs.next()) {
-                Emprestimo emprestimo = new Emprestimo();
-                emprestimo.setId(rs.getInt("id_emprestimo"));
-                emprestimo.setIdFerramenta(rs.getInt("id_ferramenta"));
-                emprestimo.setIdAmigo(rs.getInt("id_amigo"));
-                emprestimo.setDataInicial(toLocalDate(rs.getDate("data_inicial")));
-                emprestimo.setDataPrazo(toLocalDate(rs.getDate("data_prazo")));
-                emprestimo.setDataDevolucao(toLocalDate(rs.getDate("data_devolucao")));
-                emprestimo.setNomeFerramenta(rs.getString("nome_ferramenta"));
-                emprestimo.setNomeAmigo(rs.getString("nome_amigo"));
-                emprestimo.setApelidoAmigo(rs.getString("apelido"));
-                emprestimos.add(emprestimo);
-            }
-    
-        } catch (SQLException e) {
-            throw new ExceptionDAO("Erro ao consultar emprestimo: " + e);
-        }
-    
-        return emprestimos;
+        return buscarEmprestimos(sql);
     }
-    
-    
 
+    public ArrayList<Emprestimo> buscarAtrasados() throws ExceptionDAO {
+        String sql = "SELECT emprestimos.id as id_emprestimo, " +
+                "ferramentas.id as id_ferramenta, " +
+                "amigos.id as id_amigo, " +
+                "data_inicial, " +
+                "data_prazo, " +
+                "data_devolucao, " +
+                "ferramentas.nome AS nome_ferramenta, " +
+                "amigos.nome AS nome_amigo, " +
+                "apelido " +
+                "FROM emprestimos " +
+                "JOIN ferramentas ON emprestimos.id_ferramenta = ferramentas.id " +
+                "JOIN amigos ON emprestimos.id_amigo = amigos.id " +
+                "WHERE data_prazo < CURRENT_DATE AND data_devolucao IS NULL " +
+                "ORDER BY data_prazo ASC;";
+        return buscarEmprestimos(sql);
+    }
 
     public void cadastrar(Emprestimo emprestimo) throws ExceptionDAO {
         String sql = "INSERT INTO emprestimos (id_ferramenta, id_amigo, data_inicial, data_prazo) VALUES (?, ?, ?, ?);";
@@ -295,7 +253,7 @@ public class EmprestimoDAO implements Dao<Emprestimo> {
     public static void testarBuscarVencidos() {
         try {
             EmprestimoDAO emprestimoDAO = new EmprestimoDAO();
-            ArrayList<Emprestimo> emprestimosVencidos = emprestimoDAO.buscarVencidos();
+            ArrayList<Emprestimo> emprestimosVencidos = emprestimoDAO.buscarAtrasados();
             if (!emprestimosVencidos.isEmpty()) {
                 System.out.println("Emprestimos vencidos encontrados:");
                 for (Emprestimo emprestimo : emprestimosVencidos) {
