b10a5cb6fb3d8f0526fd6d138133af632b306815
caetanoRoque <caetano.roque17@gmail.com>
2024-05-18 16:49:10 -0300

    refactor: adiciona função comum para buscar amigo

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -29,26 +29,7 @@ public class AmigoDAO implements DAO<Amigo> {
 
     public ArrayList<Amigo> buscarTudo() throws ExceptionDAO {
         String sql = "SELECT * FROM amigos ORDER BY nome ASC;";
-        ArrayList<Amigo> amigos = new ArrayList<>();
-
-        try (Connection conn = new DBConexao().getConexao();
-                PreparedStatement pStatement = conn.prepareStatement(sql);
-                ResultSet rs = pStatement.executeQuery()) {
-
-            while (rs.next()) {
-                Amigo amigo = new Amigo();
-                amigo.setId(rs.getInt("id"));
-                amigo.setNome(rs.getString("nome"));
-                amigo.setApelido(rs.getString("apelido"));
-                amigo.setTelefone(rs.getString("telefone"));
-                amigos.add(amigo);
-            }
-
-        } catch (SQLException e) {
-            throw new ExceptionDAO("Erro ao consultar amigo: " + e);
-        }
-
-        return amigos;
+        return buscarAmigos(sql);
     }
 
     public void cadastrar(Amigo amigo) throws ExceptionDAO {
@@ -164,13 +145,15 @@ public class AmigoDAO implements DAO<Amigo> {
 
     public ArrayList<Amigo> buscarNome(String nome) throws ExceptionDAO {
         String sql = "SELECT * FROM amigos WHERE UPPER(nome) LIKE UPPER(?)";
+        return buscarAmigos(sql);
+    }
+    
+    private ArrayList<Amigo> buscarAmigos(String sql) throws ExceptionDAO {
         ArrayList<Amigo> amigos = new ArrayList<>();
         
         try (Connection conn = new DBConexao().getConexao();
-             PreparedStatement pStatement = conn.prepareStatement(sql)) {
-            
-            pStatement.setString(1, "%" + nome + "%");
-            ResultSet rs = pStatement.executeQuery();
+             PreparedStatement pStatement = conn.prepareStatement(sql);
+             ResultSet rs = pStatement.executeQuery()) {
             
             while (rs.next()) {
                 Amigo amigo = new Amigo();
@@ -187,6 +170,4 @@ public class AmigoDAO implements DAO<Amigo> {
         
         return amigos;
     }
-    
-    //TODO: implementar testes
 }
