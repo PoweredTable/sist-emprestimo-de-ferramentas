053b2202c04443137f8be27b5e00a7a68458eeba
caetanoRoque <caetano.roque17@gmail.com>
2024-05-19 16:25:07 -0300

    refactor: refatora quantidadeAmigos do DAO

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -190,20 +190,21 @@ public class AmigoDAO implements DAO<Amigo> {
     
     public int quantidadeAmigos() throws ExceptionDAO {
         String sql = "SELECT COUNT(*) FROM amigos";
-    
+        int quantidade = 0;
+
         try (Connection conn = new DBConexao().getConexao();
-             PreparedStatement pStatement = conn.prepareStatement(sql);
-             ResultSet rs = pStatement.executeQuery()) {
-    
+                PreparedStatement pStatement = conn.prepareStatement(sql);
+                ResultSet rs = pStatement.executeQuery()) {
+
             if (rs.next()) {
-                return rs.getInt(1);
+                quantidade = rs.getInt(1);
             }
-    
+
         } catch (SQLException e) {
-            throw new ExceptionDAO("Erro ao consultar quantidade de amigos: " + e);
+            throw new ExceptionDAO("Erro ao consultar quantidade de ferramentas: " + e);
         }
-    
-        return 0;
+
+        return quantidade;
     }
 
     
