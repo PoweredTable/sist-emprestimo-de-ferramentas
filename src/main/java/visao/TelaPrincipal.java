831c87a66016e758c2aadb1eeb5fe23a5535572d
PoweredTable <poweredlucas@gmail.com>
2024-05-25 19:46:00 -0300

    chore: remove duplicatas e prints

--- src/main/java/visao/TelaPrincipal.java
+++ src/main/java/visao/TelaPrincipal.java
@@ -368,7 +368,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        TelaAmigos amigos = new TelaAmigos();
+        TelaAmigo amigos = new TelaAmigo();
         amigos.setVisible(true);
         setEnabled(false);
         amigos.setLocationRelativeTo(null);
