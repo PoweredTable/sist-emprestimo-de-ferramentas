d53420a48c546cad3ef489fa00517bcf3d8dea40
andreynaza <andreynaz@live.com>
2024-06-08 14:32:30 -0300

    docs: documenta classe Dao.java

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -5,51 +5,31 @@ import java.util.Optional;
 
 /**
  * Interface que define os métodos básicos de um DAO (Data Access Object).
- *
- * @param <T> O tipo de objeto que o DAO manipula.
  */
 public interface DAO<T> {
 
     /**
      * Busca um objeto pelo ID.
-     *
-     * @param id ID do objeto a ser buscado.
-     * @return Optional contendo o objeto encontrado ou vazio se não encontrado.
-     * @throws ExceptionDAO Exceção lançada pela camada DAO.
      */
     Optional<T> buscar(Integer id) throws ExceptionDAO;
 
     /**
      * Busca todos os objetos.
-     *
-     * @return Lista de todos os objetos.
-     * @throws ExceptionDAO Exceção lançada pela camada DAO.
      */
     ArrayList<T> buscarTudo() throws ExceptionDAO;
 
     /**
      * Cadastra um novo objeto.
-     *
-     * @param t Objeto a ser cadastrado.
-     * @throws ExceptionDAO Exceção lançada pela camada DAO.
      */
     void cadastrar(T t) throws ExceptionDAO;
 
     /**
      * Altera um objeto existente.
-     *
-     * @param t Objeto a ser alterado.
-     * @return Número de linhas afetadas.
-     * @throws ExceptionDAO Exceção lançada pela camada DAO.
      */
     int alterar(T t) throws ExceptionDAO;
 
     /**
      * Exclui um objeto pelo ID.
-     *
-     * @param id ID do objeto a ser excluído.
-     * @return Número de linhas afetadas.
-     * @throws ExceptionDAO Exceção lançada pela camada DAO.
      */
     int excluir(Integer id) throws ExceptionDAO;
 }
