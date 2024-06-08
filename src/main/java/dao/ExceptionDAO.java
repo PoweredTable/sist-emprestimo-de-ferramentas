a5c4635abc216e2c6d6d0997daa41766c47b0045
andreynaza <andreynaz@live.com>
2024-06-08 14:33:53 -0300

    docs: documenta classe ExceptionDAO.java

--- src/main/java/dao/ExceptionDAO.java
+++ src/main/java/dao/ExceptionDAO.java
@@ -2,16 +2,11 @@ package dao;
 
 /**
  * Classe ExceptionDAO
- * Esta classe representa exceções específicas relacionadas ao DAO (Data Access Object).
- * 
- * @autor caeta
  */
 public class ExceptionDAO extends Exception {
     
     /**
      * Construtor da classe ExceptionDAO
-     * 
-     * @param mensagem A mensagem de erro associada à exceção
      */
     public ExceptionDAO(String mensagem) {
         super(mensagem);
